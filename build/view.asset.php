7a36e17efe2d3edf50280b753f42670bf5c605f9
atarac <atara.c.daisuke@gmail.com>
2023-09-02 22:19:08 +0900

    はじめてのコミット

---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'e5de1404333b7a6da43e');
---
+++ src/edit.js
@@ -0,0 +1,57 @@
+import { __ } from '@wordpress/i18n';
+import { useBlockProps,RichText } from '@wordpress/block-editor';
+import { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
+import { Inner } from './inner';
+import './editor.scss';
+
+export default function Edit( { attributes, setAttributes } ) {
+	const blockProps = useBlockProps();
+	const onChangeContent = ( newContent ) => {
+		setAttributes( { content:newContent } )
+		console.log("content attribute updated:", newContent);
+	};
+	const [ showInner, setShowInner ] = useState( false );
+	const toggleInner = () => {
+		setShowInner( !showInner );
+	};
+	const [ accordionClass, setAcordionClass] = useState('accordion-inner-exit-done');
+	const updateAccorionClass = ( status ) => {
+		if( status === 'entered' ) {
+			setAcordionClass('accordion-inner-enter-done');
+		} else if( status === 'exited') {
+			setAcordionClass('accordion-inner-exit-done');
+		};
+	};
+	const onChangeInnerContent = ( newContent ) => {
+		setAttributes( { innerContent:newContent } );
+	};
+
+	return (
+		<>
+			<div onClick={ toggleInner }>
+				<RichText
+					{ ...blockProps }
+					tagName='p'
+					onChange={ onChangeContent }
+					allowedFormats={ [ 'core/bold', 'core/italic' ] }
+					value={ attributes.content }
+					placeholder={ __( 'Write your text...' ) }
+				/>
+			</div>
+			<CSSTransition
+				in={ showInner }
+				timeout={ 500 }
+				classNames="accordion-inner"
+				onEntered={ () => updateAccorionClass('entered') }
+				onExited={ () => updateAccorionClass('exited') }
+			>
+				<Inner
+					innerContent={ attributes.innerContent }
+					setInnerContent={ onChangeInnerContent }
+					accordionClass={ accordionClass }
+				/>
+			</CSSTransition>
+		</>
+	);
+};
